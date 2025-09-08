34f391dd43adec754c7a842427e03098a1d7ee81
leloeduk <leloeduk2025@gmail.com>
2025-09-08 23:50:11 +0100

    feat:add app_theme.dart

---
+++ lib/presentation/bloc/theme/app_themes.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class AppThemes {
+  static final ThemeData lightTheme = ThemeData(
+    brightness: Brightness.light,
+    primaryColor: Colors.blue,
+    scaffoldBackgroundColor: Colors.grey[100],
+    appBarTheme: const AppBarTheme(
+      backgroundColor: Colors.blue,
+      foregroundColor: Colors.white,
+      elevation: 2,
+    ),
+    floatingActionButtonTheme: const FloatingActionButtonThemeData(
+      backgroundColor: Colors.blue,
+      foregroundColor: Colors.white,
+    ),
+    textTheme: const TextTheme(
+      bodyLarge: TextStyle(color: Colors.black87),
+      bodyMedium: TextStyle(color: Colors.black54),
+    ),
+    colorScheme: ColorScheme.fromSwatch().copyWith(secondary: Colors.amber),
+  );
+
+  static final ThemeData darkTheme = ThemeData(
+    brightness: Brightness.dark,
+    primaryColor: Colors.deepPurple,
+    scaffoldBackgroundColor: Colors.black,
+    appBarTheme: const AppBarTheme(
+      backgroundColor: Colors.deepPurple,
+      foregroundColor: Colors.white,
+      elevation: 2,
+    ),
+    floatingActionButtonTheme: const FloatingActionButtonThemeData(
+      backgroundColor: Colors.deepPurple,
+      foregroundColor: Colors.white,
+    ),
+    textTheme: const TextTheme(
+      bodyLarge: TextStyle(color: Colors.white),
+      bodyMedium: TextStyle(color: Colors.white70),
+    ),
+    colorScheme: ColorScheme.fromSwatch(
+      brightness: Brightness.dark,
+    ).copyWith(secondary: Colors.tealAccent),
+  );
+}
